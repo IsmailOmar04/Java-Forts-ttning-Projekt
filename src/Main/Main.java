a2c708bd0edc208e27833fa097b83e017fd99df2
bile <bile0527@gmail.com>
2025-03-11 16:23:14 +0100

    test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,29 +1,19 @@
 package Main;
-import Ekonomi.EkonomiController;
-import Lager.LagerController;
 import Login.LoginController;
 import Kunder.KunderController;
-import Order.OrderController;
 import Produkter.ProduktController;
 
 public class Main {
     public static void main(String[] args) {
 
         LoginController loginController = new LoginController();
-        OrderController orderController = new OrderController();
+
         KunderController kunderController = new KunderController();
        ProduktController produktController = new ProduktController();
-        LagerController lagerController = new LagerController();
-        EkonomiController ekonomiController = new EkonomiController();
 
-
-        //ekonomiController.run();
-       //lagerController.run();
-      // orderController.run();
-      //produktController.run();
-     //loginController.run();
-     //kunderController.run();
-
+       //produktController.run();
+     loginController.run();
+     kunderController.run();
 
     }
 
